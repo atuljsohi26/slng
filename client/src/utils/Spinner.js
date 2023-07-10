f36b995c72ba427eb9573bfb0b6f53b66ca2fe6f
Atul <developerjoshi26@gmail.com>
2023-07-10 23:21:42 +0530

    add loader

--- client/src/utils/Spinner.js
+++ client/src/utils/Spinner.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { Bars } from "react-loader-spinner";
+import { FidgetSpinner } from "react-loader-spinner";
 
 const Spinner = ({ message, height, width, color, messageColor }) => {
   return (
-    <div className="flex flex-col justify-center items-center w-full relative">
-      <div className="absolute backdrop-blur-3xl">
-        <Bars color={color} height={height} width={width} className="m-5" />
-        <p style={{ color: messageColor }} className="text-lg text-center px-2">
-          {message}
-        </p>
-      </div>
+    <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center z-50 backdrop-filter backdrop-blur">
+      <FidgetSpinner
+        color={color}
+        height={height}
+        width={width}
+        className="m-5"
+      />
+      <p style={{ color: messageColor }} className="text-lg text-center">
+        {message}
+      </p>
     </div>
   );
 };
